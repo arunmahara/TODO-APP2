10501f1ddd7b45882d382026d71441a4487ba46d
arunmahara <arunmahara124@gmail.com>
2022-07-14 19:22:22 +0545

    update todo task

--- todo/views.py
+++ todo/views.py
@@ -10,10 +10,14 @@ def index(request):
     if request.method == 'POST':
         todotask = request.POST['task']
 
-        todo = Todo(task=todotask)
-        todo.save()
-        messages.info(request, 'Successfully Added')
-        return redirect('/')
+        if todotask :
+            todo = Todo(task=todotask)
+            todo.save()
+            messages.info(request, 'Successfully Added')
+            return redirect('/')
+        else:
+            messages.info(request, 'Please enter task')
+            return redirect('/')
     
     else:
         alltask = Todo.objects.all()
@@ -24,16 +28,17 @@ def deleteTask(request, id):
     if request.method == 'POST':
         task = Todo.objects.get(pk=id)
         task.delete()
+        messages.info(request, 'Successfully Deleted')
         return redirect('/')
 
 def updateTask(request, id):
-    # if request.method == 'POST':
-    #     task = Todo.objects.get(pk=id)
-    # else:
-    task = Todo.objects.get(pk=id)
-    
-
-    
-    return render(request, 'todo/update.html', {'tasks': task} )
+    if request.method == 'POST':
+        tasks = Todo.objects.get(pk=id)
+        tasks.task = request.POST['task']
+        tasks.save()
+        messages.info(request, 'Successfully Updated')
+    else:
+        tasks = Todo.objects.get(pk=id)
+    return render(request, 'todo/update.html', {'tasks': tasks} )
 
     
