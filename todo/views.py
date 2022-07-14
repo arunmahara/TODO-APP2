4f6a026cdc68321da48ff977040e5ca00c923b01
arunmahara <arunmahara124@gmail.com>
2022-07-14 15:51:01 +0545

    add/show todo task

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Todo
 
 # Create your views here.
 def index(request):
     # return (HttpResponse('<h1> TODO</h1>') )
+    if request.method == 'POST':
+        todotask = request.POST['task']
+
+        todo = Todo(task=todotask)
+        todo.save()
+        messages.info(request, 'Successfully Added')
+        return redirect('/')
     
-    return render(request, 'todo/index.html')
+    else:
+        alltask = Todo.objects.all()
+        return render(request, 'todo/index.html', {'alltasks': alltask})
