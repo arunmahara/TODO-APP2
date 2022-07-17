a9b7cb4e532981d8511ad074df60e06f6935b1aa
arunmahara <arunmahara124@gmail.com>
2022-07-18 01:01:16 +0545

    status function

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,11 @@
+from pickle import FALSE, TRUE
 from django.db import models
+from xmlrpc.client import boolean
+
 
 # Create your models here.
 
 class Todo(models.Model):
     task = models.TextField()
-
+    status = models.BooleanField(default=False) 
+    date = models.DateTimeField(auto_now=True)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -5,40 +6,48 @@ from django.contrib import messages
 from .models import Todo
 
 # Create your views here.
+# function to show and save todo task
 def index(request):
-    # return (HttpResponse('<h1> TODO</h1>') )
     if request.method == 'POST':
         todotask = request.POST['task']
 
-        if todotask :
+        if todotask : #checks if task-field is empty
             todo = Todo(task=todotask)
-            todo.save()
+            todo.save() #save task
             messages.info(request, 'Successfully Added')
             return redirect('/')
         else:
-            messages.info(request, 'Please enter task')
+            messages.info(request, 'Please enter task') #shows this messasge if task-field is empty
             return redirect('/')
     
     else:
-        alltask = Todo.objects.all()
+        alltask = Todo.objects.all().order_by('-date') #get all todo tasks according to date created created
         return render(request, 'todo/index.html', {'alltasks': alltask})
 
-
+# function to delete todo task
 def deleteTask(request, id):
     if request.method == 'POST':
-        task = Todo.objects.get(pk=id)
-        task.delete()
+        task = Todo.objects.get(pk=id)  #get task of requested id
+        task.delete()  #delete task of request id
         messages.info(request, 'Successfully Deleted')
         return redirect('/')
 
+# function to update todo task
 def updateTask(request, id):
     if request.method == 'POST':
-        tasks = Todo.objects.get(pk=id)
-        tasks.task = request.POST['task']
-        tasks.save()
+        tasks = Todo.objects.get(pk=id) #get task of requested id
+        tasks.task = request.POST['task'] #update task
+        status = request.POST.get('status') #get value from status checkbox (on/none)
+
+        if status  == 'on':  #checks if status checkbox is checked or not
+            tasks.status = True
+        else:
+            tasks.status = False
+            
+        tasks.save() #save updated task
         messages.info(request, 'Successfully Updated')
     else:
-        tasks = Todo.objects.get(pk=id)
+        tasks = Todo.objects.get(pk=id) #get task of requested id
     return render(request, 'todo/update.html', {'tasks': tasks} )
 
     
