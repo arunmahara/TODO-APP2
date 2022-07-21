e16dc73d2b1aa437a28b512d4f5e2c81f634a584
arunmahara <arunmahara124@gmail.com>
2022-07-22 01:56:15 +0545

    added counter

--- todo/admin.py
+++ todo/admin.py
@@ -4,4 +4,11 @@ from django.contrib import admin
 from .models import Todo
 
 # Register your models here.
-admin.site.register(Todo)
+
+# admin.site.register(Todo) # doesn't show todos in tabular format in admin panel
+
+@admin.register(Todo) #registering model using decorators
+class TodoAdmin(admin.ModelAdmin):  #for showing todos in tabular format in admin panel
+    list_display = ('id', 'task', 'status', 'date')
+
+# admin.site.register(Todo, TodoAdmin) #registering model without using decorators
--- todo/models.py
+++ todo/models.py
@@ -9,3 +9,7 @@ class Todo(models.Model):
     task = models.TextField()
     status = models.BooleanField(default=False) 
     date = models.DateTimeField(auto_now=True)
+
+    # def __str__(self):   # to show task name in admin panel instead of TODO object(1)  # no need of this if class is created in admin.py for showing data in table
+    #     return (self.task)    # for id or integer => return str(self.task)
+
